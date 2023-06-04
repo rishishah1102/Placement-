2ed270242347eb16d28eee9959a18bbfaf197dcc
rishishah1102 <rishi.shah545@gmail.com>
2023-06-04 12:43:32 +0530

    modified problem14

--- LeetCode/Problem14.java
+++ LeetCode/Problem14.java
@@ -14,22 +14,15 @@ public class Problem14 {
 
         for (int i = 0; i < s[0].length(); i++) {
             for (int j = 0; j < s.length; j++) {
-                // System.out.println(j + " " + i);
                 String temp = s[j];
-                // System.out.println("temp11" + temp);
 
                 if (j == 0) {
                     temp2 = temp.charAt(index);
-                    // System.out.println("temp21" + temp2);
-                    // index++;
                 } else if (i == temp.length()) {
                     issame = false;
                     break;
                 } else {
-                    // System.out.println("temp22: " + temp2);
-                    // System.out.println("temp12: " + temp.substring(0, i + 1));
                     char temp3 = temp.charAt(index);
-                    // System.out.println(temp3);
 
                     if (temp2 != temp3) {
                         issame = false;
