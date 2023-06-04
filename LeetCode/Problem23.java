9b84ced53b47c69837b9dd664081b33c64be2126
rishishah1102 <rishi.shah545@gmail.com>
2023-06-04 15:23:04 +0530

    added problem23

---
+++ LeetCode/Problem23.java
@@ -0,0 +1,68 @@
+package LeetCode;
+
+import java.util.PriorityQueue;
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Problem23 {
+    public static ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        // Add the heads of all linked lists to the min heap
+        for (ListNode list : lists) {
+            if (list != null) {
+                minHeap.offer(list);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        // Merge the lists by repeatedly extracting the smallest node from the min heap
+        while (!minHeap.isEmpty()) {
+            ListNode smallest = minHeap.poll();
+            tail.next = smallest;
+            tail = tail.next;
+
+            if (smallest.next != null) {
+                minHeap.offer(smallest.next);
+            }
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        // Create three sorted linked lists
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(4);
+        list1.next.next = new ListNode(5);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode list3 = new ListNode(2);
+        list3.next = new ListNode(6);
+
+        // Add the lists to an array
+        ListNode[] lists = { list1, list2, list3 };
+
+        // Merge the sorted linked lists
+        ListNode mergedList = mergeKLists(lists);
+
+        // Print the merged list
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " ");
+            mergedList = mergedList.next;
+        }
+    }
+}
